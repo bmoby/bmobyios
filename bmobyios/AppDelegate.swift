7521c14f5f1136c909cf749a4374671dcaa235d2
bmoby <msouleymanov@gmail.com>
2016-10-05 12:02:29 +0200

    bmobyios first commit configuration of parse and adding frameworks to the project is done

--- bmobyios/AppDelegate.swift
+++ bmobyios/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        // CONFIGURATION OF PARSE
+        let parseConfig = ParseClientConfiguration { (ParseMutableClientConfiguration) in
+            
+            //Accessing heroku App vio ID & Keys
+            ParseMutableClientConfiguration.applicationId = "bmobyiosOS1107198730091989"
+            ParseMutableClientConfiguration.clientKey = "bmobyKeyBmobyiOsOS1107198730091989"
+            ParseMutableClientConfiguration.server = "http://bmobyios.herokuapp.com/parse"
+        }
+        
+        Parse.initializeWithConfiguration(parseConfig)
+
         return true
     }
 
---
+++ bmobyios/connectionVC.swift
@@ -0,0 +1,38 @@
+//
+//  connectionVC.swift
+//  bmobyios
+//
+//  Created by Magomed Souleymnov on 05/10/2016.
+//  Copyright © 2016 bmoby. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+class connectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
