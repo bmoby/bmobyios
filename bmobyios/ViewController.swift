e6f3b53e328ef942368182e987335d7f7657e479
bmoby <msouleymanov@gmail.com>
2016-10-05 10:55:20 +0200

    Initial Commit

---
+++ bmobyios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bmobyios
+//
+//  Created by Magomed Souleymnov on 05/10/2016.
+//  Copyright © 2016 bmoby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
